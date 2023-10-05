53ac517a0cfce590f8f1f6ecdece14e94a6fff28
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2023-10-05 15:07:55 +0900

    添加 自动购买 蓝苹果 功能
    仅限 JP

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ authKeys = os.environ['authKeys'].split(',')
 secretKeys = os.environ['secretKeys'].split(',')
 fate_region = os.environ['fateRegion']
 webhook_discord_url = os.environ['webhookDiscord']
+blue_apple_cron = os.environ.get("MAKE_BLUE_APPLE")
+
+
 UA = os.environ['UserAgent']
 
-if UA != 'nullvalue':
+if UA:
     fgourl.user_agent_ = UA
 
 userNums = len(userIds)
@@ -24,6 +27,19 @@ secretKeyNums = len(secretKeys)
 
 logger = logging.getLogger("FGO Daily Login")
 coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+
+
+def check_blue_apple_cron(instance):
+    if blue_apple_cron:
+
+        cron = croniter(blue_apple_cron)
+        next_date = cron.get_next(datetime)
+        current_date = datetime.now()
+        
+        if current_date >= next_date:
+            logger.info('Trying buy one blue apple!')
+            instance.buyBlueApple(1)
+            time.sleep(2)
 
 
 def get_latest_verCode():
@@ -49,14 +65,25 @@ def main():
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
-                logger.info('Loggin into account!')
+                logger.info('登录账号!')
                 instance.topLogin()
                 time.sleep(2)
                 instance.topHome()
                 time.sleep(2)
-                logger.info('Throw daily friend summon!')
+                logger.info('开始友情点召唤!!')
                 instance.drawFP()
                 time.sleep(2)
+                check_blue_apple_cron(instance)
+                logger.info('尝试购买蓝苹果!')
+                try:
+                   instance.buyBlueApple(1)
+                   time.sleep(2)
+                   for _ in range(3):
+                      instance.buyBlueApple(1)
+                      time.sleep(2)
+                except Exception as ex:
+                    logger.error(ex)
+                    
             except Exception as ex:
                 logger.error(ex)
 
