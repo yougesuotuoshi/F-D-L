ee6083574ff394d706ced14c8b60ea764c79de0f
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2024-05-20 18:09:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ authKeys = os.environ['authKeys'].split(',')
 secretKeys = os.environ['secretKeys'].split(',')
 fate_region = os.environ['fateRegion']
 webhook_discord_url = os.environ['webhookDiscord']
-blue_apple_cron = os.environ.get("MAKE_BLUE_APPLE")
 idempotency_key_signature = os.environ.get('IDEMPOTENCY_KEY_SIGNATURE_SECRET')
 device_info = os.environ.get('DEVICE_INFO_SECRET')
 user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
@@ -23,17 +22,6 @@ secretKeyNums = len(secretKeys)
 
 logger = logging.getLogger("FGO Daily Login")
 coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
-
-def check_blue_apple_cron(instance):
-    if blue_apple_cron:
-
-        cron = croniter(blue_apple_cron)
-        next_date = cron.get_next(datetime)
-        current_date = datetime.now()
-        
-        if current_date >= next_date:
-            instance.buyBlueApple(1)
-            time.sleep(2)
 
 def get_latest_verCode():
     endpoint = "https://raw.githubusercontent.com/DNNDHH/FGO-VerCode-extractor/JP/VerCode.json"
@@ -61,17 +49,10 @@ def main():
                 instance.lq001()
                 instance.lq002()
                 time.sleep(2)
-                check_blue_apple_cron(instance)
-                logger.info(f"\n ======================================== \n [+] 尝试购买蓝苹果 \n ======================================== " )
-                try:
-                   instance.buyBlueApple(1)
-                   time.sleep(2)
-                   for _ in range(3):  # 默认购买3个蓝苹果 ，需要 （120AP  3青銅树苗）
-                      instance.buyBlueApple(1)
-                      time.sleep(2)
-                       
-                except Exception as ex:
-                    logger.error(ex)
+                instance.buyBlueApple()
+                time.sleep(1)
+                instance.drawFP()
+
 
             except Exception as ex:
                 logger.error(ex)
